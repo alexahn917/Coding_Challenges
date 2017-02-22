717bb5d63776489ff421c20c66f030632a1994c6
Alex Ahn <alexahn917@gmail.com>
2017-02-22 12:49:50 -0500

    Couple more

---
+++ src/Array/perm_integer_just_before.java
@@ -0,0 +1,25 @@
+package Array;
+
+import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
+
+public class perm_integer_just_before {
+	public static void main(String[] args) {
+		System.out.println(justSmaller(1432));
+	}
+	
+	public static int justSmaller(int num) {
+		char[] str_num = (num + "").toCharArray();
+		int len = str_num.length;
+		int last = Character.getNumericValue(str_num[len-1]);
+		int curr = 0;
+		for (int i=str_num.length-2; i>0; i--) {
+			curr = Character.getNumericValue(str_num[i]);
+			if (curr > last) {
+				str_num[len-1] = (char)(curr + '0');
+				str_num[i] = (char)(last + '0');
+				break;
+			}
+		}
+		return Integer.parseInt(new String(str_num));
+	}
+}
---
+++ src/String/permutations.java
@@ -0,0 +1,23 @@
+package String;
+
+public class permutations {
+	
+	
+	public static void main(String[] args) {
+		printPermutations("abcd");
+	}
+	
+	public static void printPermutations(String str) {
+		printPermutationsRecursive("", str);
+	}
+	
+	public static void printPermutationsRecursive(String prefix, String str) {
+		int n = str.length();
+		if (n == 0) {
+			System.out.println(prefix);
+		}
+		for (int i = 0; i < n; i++) {
+			printPermutationsRecursive(prefix + str.charAt(i), str.substring(0,i) + str.substring(i+1, n));
+		}		
+	}
+}
